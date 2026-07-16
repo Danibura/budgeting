475e250c9d965e18b85ec7b5f1f25f94f03c0969
Daniele Buratti <danibura2007@gmail.com>
2026-07-16 14:43:10 +0200

    Query from server component, not by calling API

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
 import Header from "@/components/header";
 import TransactionsList from "@/components/transactionsList";
+import { db } from "@/db/drizzle";
+import { transactions } from "@/db/schema";
+import type { Transaction } from "@/types/types";
 
 export default async function TransactionsPage() {
+  const result = (await db.select().from(transactions)) as Transaction[];
+
   return (
     <div className="bg-stone-50 min-h-screen">
       <Header title="Transactions"></Header>
@@ -17,7 +22,7 @@ export default async function TransactionsPage() {
           </Link>
         </div>
         <div className="p-4 w-full">
-          <TransactionsList />
+          <TransactionsList initialTransactions={result} />
         </div>
       </div>
     </div>
--- components/transactionsList.tsx
+++ components/transactionsList.tsx
@@ -4,23 +4,13 @@ import type { Transaction } from "@/types/types";
 import TransactionElement from "./transactionElement";
 import { useEffect, useState } from "react";
 
-export default function TransactionsList() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-  useEffect(() => {
-    loadTransactions();
-  }, []);
-
-  async function loadTransactions() {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_URL}/api/transactions/`,
-      {
-        method: "GET",
-      },
-    );
-    const transactionsData = await response.json();
-    setTransactions(transactionsData);
-  }
+export default function TransactionsList({
+  initialTransactions,
+}: {
+  initialTransactions: Transaction[];
+}) {
+  const [transactions, setTransactions] =
+    useState<Transaction[]>(initialTransactions);
 
   return (
     <div className="flex flex-col w-full   rounded-xl bg-white">
